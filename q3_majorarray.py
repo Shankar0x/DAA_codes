1171fd58cb6d4db6d32da8627046886358f6e747
Shankar0x <Shankar0x@github.com>
2022-11-23 20:52:53 +0530

    Added q3

--- q3_majorarray.py
+++ q3_majorarray.py
@@ -1,24 +1,22 @@
-def partition(arr,l,r):
-    x = arr[r]
-    i = l
-    for j in range(l,r):
-        if arr[j]<=x:
-            arr[i],arr[j] = arr[j],arr[i]
-            i+=1
-    arr[r],arr[i] = arr[i],arr[r]
-    return i
+def majorelem(arr,l,r):
+    if l == r-1:
+        return arr[l],1
+    mid = l + ((r-l)//2)
+    maj_left,extra_left = majorelem(arr,l,mid)
+    maj_right, extra_right = majorelem(arr,mid,r)
 
-def qsel(arr,l,r,k):
-    if (k > 0 and k <= r - l + 1):
-        p = partition(arr,l,r)
-        if p-l == k-1:
-            return arr[p]
-        elif p-l > k-1:
-            return qsel(arr,l,p-1,k)
-        else:
-            return qsel(arr,p+1,r,k-p+l-1)
-arr = [ 10, 4, 5, 8, 6, 11, 26 ]
-n = len(arr)
-k = 3
-print("K-th smallest element is ", end = "")
-print(qsel(arr, 0, n - 1, k))
+    if maj_left == maj_right:
+        maj = maj_left
+        extra = extra_left+extra_right
+    elif extra_right > extra_left:
+        maj = maj_right
+        extra = extra_right - extra_left
+    else:
+        maj = maj_left
+        extra = extra_left - extra_right
+    return maj,extra
+
+if __name__ == '__main__':
+    arr = [1,1,1,1,1,2,2,2,2,2,2,2,2]
+    ans,_ = majorelem(arr,0,len(arr)-1)
+    print(f"Majority element is: {ans}")
